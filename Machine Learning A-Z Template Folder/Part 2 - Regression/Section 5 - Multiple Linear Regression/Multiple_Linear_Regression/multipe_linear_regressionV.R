b9535552a6378a243c83a6623e4f72a85feba12c
STEFANINI-DOM\vbbarros <vbbarros@latam.stefanini.com>
2018-12-28 15:23:35 -0200

    Finalização da Regressão Linear Múltipla

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multipe_linear_regressionV.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multipe_linear_regressionV.R
@@ -1,16 +1,36 @@
 # Data Preprocessing Template
 
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset = read.csv('50_Startups.csv')
+
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
+               data = training_set)
+
+y_pred = predict(regressor, newdata = test_set)
+
+
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = training_set)
+
+y_pred_Back = predict(regressor, newdata = test_set)
+
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = dataset)
+
+y_pred_mark = predict(regressor, newdata = test_set)
